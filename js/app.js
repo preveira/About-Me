8df28fea2f1f5899d73a73c0c424553900758710
preveira <patrick.k.reveira@gmail.com>
2024-01-24 21:54:00 -0800

    adding functionality to prompts and added CSS & HTML

--- js/app.js
+++ js/app.js
@@ -20,63 +20,104 @@ function buttonGame() {
         "What is ranked 3rd on my Personal Interest?",
         "What do I aspire to create change through?",
         "What is my 2nd ranked Professional Interest?",
-        "Can you guess my favorite number between 15 and 25?"
+        "Can you guess my favorite number between 15 and 25?",
+        "Marshawn Lynch is 8th amongst my Top 10 best NFL Running Backs of All time. Can you tell me what his nickname was? take a first guess!",
+        "Who did I rank number 1 on my top NFL Running Backs of All time?"
     ];
 
+    let correctAnswers = [
+        "software development engineer",
+        "time with family",
+        "positivity and growth mindset",
+        "entrepreneurship",
+        "21",
+        "beast mode",
+        "Emmitt Smith"
+    ];
+
+    let answerChoices = [
+        ["software development engineer"],
+        ["time with family"],
+        ["positivity and growth mindset"],
+        ["entrepreneurship"],
+        Array.from({ length: 11 }, (_, i) => (i + 15).toString()), 
+        ["Dump Truck", "Beast Mode", "Bull Dozer", "Thrasher", "Sharknado", "Rhino Beast", "Heavy Hitter", "Big Bash", "Boom Boom Kid", "Big Nasty"],
+        ["Emmitt Smith"]
+    ];    
+ 
     let correctCount = 0;
     let wrongCount = 0;
 
-    for (let i = 0; i < questions.length - 1; i++) {
+    for (let i = 0; i < questions.length; i++) {
         let userAnswer = prompt(questions[i]);
         let lowercasedAnswer = userAnswer.toLowerCase();
-        let correctAnswer = getCorrectAnswer(i).toLowerCase();
+        let correctAnswer = correctAnswers[i].toLowerCase();
 
-        if (lowercasedAnswer === correctAnswer) {
-            let confirmation = "Nice work " + user + ", You got it!!";
-            alert(confirmation);
-            correctCount++;
+        if (i === 4) {
+            let favoriteNumber = 21;
+            let remainingTriesNumber = 10;
+
+            while (remainingTriesNumber > 0) {
+                let userGuessNumber = parseInt(userAnswer);
+                
+                if (isNaN(userGuessNumber)) {
+                    alert("Please enter a valid number.");
+                    break;
+                }
+
+                if (userGuessNumber === favoriteNumber) {
+                    alert("Yay, you guessed correctly!");
+                    correctCount++;
+                    break;
+                } else {
+                    remainingTriesNumber--;
+                    alert("Incorrect! You have " + remainingTriesNumber + " tries left.");
+            
+                    if (remainingTriesNumber > 0) {
+                        userAnswer = prompt(questions[i]);
+                    }
+                }
+            }
+        } else if (i === 5) {
+
+            let remainingTriesNickname = 6;
+        
+            while (remainingTriesNickname > 0) {
+                let userGuessNickname = prompt(
+                    questions[i] + "\nAvailable choices: " + answerChoices[i].join(", ")
+                );
+                
+                if (userGuessNickname.toLowerCase() === correctAnswer) {
+                    alert("Yay, you guessed correctly!");
+                    correctCount++;
+                    break;
+                    
+                }  else {
+                    remainingTriesNickname--;
+                    alert("Incorrect! You have " + remainingTriesNickname + " tries left.");
+
+                if (!answerChoices[i].includes(userGuessNickname)) {
+                    continue;
+                }
+                    answerChoices[i] = answerChoices[i].filter(choice => choice !== userGuessNickname);
+
+                    if (remainingTriesNumber > 0) {
+                        userAnswer = prompt(questions[i]);
+                    }
+                }
+            }         
         } else {
-            alert("Better luck on the next one :(" + user + "");
-            wrongCount++;
-        }
-    }
-
-    let favoriteNumber = 21;
-    let attempts = 10;
-
-    for (let i = 0; i < attempts; i++) {
-        let guess = prompt("Can you guess my favorite number between 15 and 25?");
-        let guessedNumber = parseInt(guess);
-
-        if (!isNaN(guessedNumber) && guessedNumber === favoriteNumber) {
-            alert("Yay, you guessed correctly! The answer is 21. Great job, " + user + "!");
-            correctCount++;
-            break;
-        } else if (i === attempts - 1) {
-            alert("Sorry, you've run out of attempts. The correct answer is 21. Better luck next time, " + user + "!");
-            wrongCount++;
-        } else {
-            alert("Incorrect guess. Try again! Attempts left: " + (attempts - i - 1));
+            if (lowercasedAnswer === correctAnswer) {
+                let confirmation = "Nice work " + user + ", You got it!!";
+                alert(confirmation);
+                correctCount++;
+            } else {
+                alert("Better luck on the next one :(" + user + "");
+                wrongCount++;
+            }
         }
     }
 
     let finalMessage = "Thanks for playing, " + user + "!\nYou got " + correctCount + " correct and " + wrongCount + " wrong answers.";
     alert(finalMessage);
-}
-
-function getCorrectAnswer(index) {
-    switch (index) {
-        case 0:
-            return "software development engineer";
-        case 1:
-            return "time with family";
-        case 2:
-            return "positivity and growth mindset";
-        case 3:
-            return "entrepreneurship";
-        case 4:
-            return "21";
-        default:
-            return "";
-    }
 }
